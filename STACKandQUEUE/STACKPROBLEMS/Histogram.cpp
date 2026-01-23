1c413ddb021c95f8a068e0d25fdd875dfa6f1169
vinyvini718@gmail.com <vinyvini718@gmail.com>
2026-01-23 19:58:50 +0530

    dsa

---
+++ STACKandQUEUE/STACKPROBLEMS/Histogram.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+int largestRectangleArea(vector<int>& heights) {
+        int n=heights.size();
+        int area=0;
+        for(int i=0;i<n;i++){
+          int count=0;
+          for(int j=i-1;j>=0;j--){
+            if(heights[j]>=heights[i])count++;
+            else
+            break;
+          }
+           for(int j=i+1;j<n;j++){
+            if(heights[j]>=heights[i])count++;
+            else
+            break;
+          }
+          area=max(area,(count+1)*heights[i]);
+        }
+        return area;
+    }
+
+    int largestRectangleAreaBetter(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> st;
+        vector<int>leftsmall(n),rightsmall(n);
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            leftsmall[i] = st.empty() ? 0 : st.top() + 1;
+            st.push(i);
+        }
+        while (!st.empty()) st.pop();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && heights[st.top()] >= heights[i]) {
+                st.pop();
+            }
+            rightsmall[i] = st.empty() ? n - 1 : st.top() - 1;
+            st.push(i);
+        }
+        int maxA = 0;
+        for (int i = 0; i < n; i++) {
+            int width = rightsmall[i] - leftsmall[i] + 1;
+            maxA = max(maxA, heights[i] * width);
+        }
+        return maxA;
+    }
